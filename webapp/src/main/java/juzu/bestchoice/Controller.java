e94570946e2ebb7aa620a92794c4813167c6d5f7
NgocTM <ngoctm@exoplatform.com>
2015-07-06 16:38:11 +0700

    Hello world. Need to remove juzu-core-xxx.jar in war/lib/ folder.

---
+++ webapp/src/main/java/juzu/bestchoice/Controller.java
@@ -0,0 +1,20 @@
+package juzu.bestchoice;
+
+import juzu.Path;
+import juzu.View;
+import juzu.Response;
+import juzu.template.Template;
+
+import javax.inject.Inject;
+import java.io.IOException;
+
+public class Controller {
+  @Inject
+  @Path("index.gtmpl")
+  Template index;
+
+  @View
+  public Response.Content index() throws IOException {
+    return index.ok();
+  }
+}
