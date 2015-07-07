4544dbb94e8a9e6219f7abfa84cc59e9030918ad
NgocTM <ngoctm@exoplatform.com>
2015-07-07 18:06:53 +0700

    Add basic pages.

--- webapp/src/main/java/juzu/bestchoice/Controller.java
+++ webapp/src/main/java/juzu/bestchoice/Controller.java
@@ -12,9 +12,37 @@ public class Controller {
   @Inject
   @Path("index.gtmpl")
   Template index;
+  
+  @Inject
+  @Path("result.gtmpl")
+  Template result;
+
+  @Inject
+  @Path("addNew.gtmpl")
+  Template addNew;
+
+  @Inject
+  @Path("viewAllComments.gtmpl")
+  Template viewAllComments;
+
 
   @View
   public Response.Content index() throws IOException {
     return index.ok();
+  }
+  
+  @View
+  public Response.Content result() throws IOException {
+    return result.ok();
+  }
+
+  @View
+  public Response.Content addNew() throws IOException {
+    return addNew.ok();
+  }
+
+  @View
+  public Response.Content viewAllComments() throws IOException {
+    return viewAllComments.ok();
   }
 }
